9f288ea1adf32a881756c0f44271222ad69a8573
alinafekamwendo <bed-com-15-17@unima.ac.mw>
2022-03-22 21:15:48 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,24 +4,42 @@ const {DB, DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
 module.exports=
 {  
   "development": {
+    "url":DATABASE_URL,
     "username": DB_USERNAME,
     "password": DB_PASSWORD,
     "database": DB,
     "host": DB_HOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "dialectOptions": {
+      ssl: {
+          rejectUnauthorized: false
+      }
+  }
   },
   "test": {
+    "url":DATABASE_URL,
     "username": DB_USERNAME,
     "password": DB_PASSWORD,
     "database": DB,
     "host": DB_HOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "dialectOptions": {
+      ssl: {
+          rejectUnauthorized: false
+      }
+  }
   },
   "production": {
+    "url":DATABASE_URL,
     "username": DB_USERNAME,
     "password": DB_PASSWORD,
     "database": DB,
     "host": DB_HOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "dialectOptions": {
+      ssl: {
+          rejectUnauthorized: false
+      }
+  }
 }
 }

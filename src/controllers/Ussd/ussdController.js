a244f1385fd1fe0b6e88e321f8e5ffd5d4b0fb41
alinafekamwendo <48163686+alinafekamwendo@users.noreply.github.com>
2022-03-23 00:27:55 +0200

    updated

--- src/controllers/Ussd/ussdController.js
+++ src/controllers/Ussd/ussdController.js
@@ -26,6 +26,7 @@ ussdController.use(express.urlencoded({extended:true}));
 
 ussdController.post('/', async (req, res,next) => {
 
+<<<<<<< HEAD
   let {sessionId,
     serviceCode, 
     phoneNumber, 
@@ -71,6 +72,63 @@ ussdController.get("/get",(req,res,next)=>{
   try {
     res.status(200).send("USSD RUNNING");
   
+=======
+const phoneNumber=req.body.phoneNumber;
+const serviceCode=req.body.serviceCode;
+const text=req.body.text;
+const sessionId=req.body.sessionId;
+const networkCode=req.body.networkCode;
+  try {
+    let {sessionId, serviceCode, phoneNumber, text} = req.body
+    if (text == '') {
+      console.log(req.body);
+      // This is the first request. Note how we start the response with CON
+      let response = `CON WELCOME TO IWETA
+      1. CHICHEWA
+      2. ENGLISH`
+      res.send(response)
+      //console.log(`welcome text ${text}`);
+    } else if (text == '1') {
+      // Business logic for first level response
+      let response = `CON YAMBANI
+       Lowetsani nambala ya chinsinsi`
+      res.send(response)
+      //console.log(`chichewa text ${text}`);
+    } else if (text == '2') {
+  
+        let response = `CON LETS START
+        Enter your ID`
+        res.send(response)
+       // console.log(`english text ${text}`);
+     
+      
+    
+      // Business logic for first level response
+      
+      
+    } else if (text == '1*1') {
+      // Business logic for first level response
+      // This is a terminal request. Note how we start the response with END
+      let response = `CON  Sakhani khola
+      1.Lilongwe khola
+      2.Chinamwali khola
+      3.Matawale`
+      res.send(response)
+      //console.log(`kusakha khola text ${text}`);
+    } else if (text == '1*2') {
+      // This is a second level response where the user selected 1 in the first instance
+      // This is a terminal request. Note how we start the response with END
+      let response = `CON Choose khola
+      1.Lilongwe
+      2.Chinamwali
+      3.Matawale`
+      res.send(response)
+      console.log(`choosing khola text ${text}`);
+    } else {
+     // console.log(text);
+      res.status(400).send('Bad request!')
+    }
+>>>>>>> parent of f0ab576 (Update ussdController.js)
   } catch (error) {
     next(error);
   }

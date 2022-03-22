67dd34ba9d53cccde0c1f227b2bc1c32e8b700b8
alinafekamwendo <48163686+alinafekamwendo@users.noreply.github.com>
2022-03-22 22:46:01 +0200

    Delete src directory

--- src/controllers/Posts/Delete.controller.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const deleteRouter = express.Router();
-const { Posts, Likes } = require("../../models");
-const { validateToken } = require("../../config/middlewares/AuthMiddleware");
-
-
-  module.exports=deleteRouter;
--- src/routes/PushNotificationsRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const pushNotificationsRoute = express.Router();
-const pushNotififcationsController = require("../controllers/Notifications/pushNotificationsController");
-
-pushNotificationsRoute.get("/",pushNotififcationsController);
-
-
-module.exports = pushNotificationsRoute;

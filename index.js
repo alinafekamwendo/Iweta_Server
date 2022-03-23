07c2972a6261763ad82aff15e17679690bfd6c0e
alinafekamwendo <48163686+alinafekamwendo@users.noreply.github.com>
2022-03-23 09:09:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.use((error,req,res,next)=>{
   });
   next(error);
 });
-const   PORT=process.env.PORT||3001;
+const   PORT=process.env.PORT||5000;
 db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port http://localhost:${PORT}`);

926fa45a85cfd3719a247abd124c2c22d3651019
alinafekamwendo <bed-com-15-17@unima.ac.mw>
2022-03-25 17:51:49 +0200

    Update Product.js

--- src/models/Product.js
+++ src/models/Product.js
@@ -24,7 +24,7 @@ module.exports = (sequelize, DataTypes) => {
       },
       Supplier:{
         type:DataTypes.STRING,
-        allowNull:false,
+        allowNull:true,
       },
      
     });
